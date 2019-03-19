871cca366e020a397b8ac0cdc92a0dcd754c3ee2
Brad Smith <brad@smithwebtek.com>
2019-03-19 09:03:33 -0400

    Update travel-site

---
+++ travel-site/gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+  watch = require('gulp-watch');
+
+gulp.task('default', function () {
+  console.log('hooray task');
+});
+
+gulp.task('html', function () {
+  console.log('something html useful here ')
+})
+
+
+gulp.task('styles', function () {
+  console.log('Imagine Sass or PostCSS tasks running here.')
+})
+
+
+
+gulp.task('watch', function () {
+  watch('./app/index.html', function () {
+    gulp.start('html');
+  })
+
+  watch('./app/assets/styles/**/*.css', function () {
+    gulp.start('styles');
+  })
+});
