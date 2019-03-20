01bd3cd575f7ac9ef2a2e59d3ef89a66c0542bb6
Brad Smith <brad@smithwebtek.com>
2019-03-20 08:44:58 -0400

    Restart series

--- travel-site/gulpfile.js
+++ travel-site/gulpfile.js
@@ -1,5 +1,12 @@
+
+
 var gulp = require('gulp'),
-  watch = require('gulp-watch');
+  watch = require('gulp-watch'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  cssvars = require('postcss-simple-vars'),
+  nested = require('postcss-nested'),
+  cssImport = require('postcss-import');
 
 gulp.task('default', function () {
   console.log('hooray task');
@@ -9,12 +16,11 @@ gulp.task('html', function () {
   console.log('something html useful here ')
 })
 
-
 gulp.task('styles', function () {
-  console.log('Imagine Sass or PostCSS tasks running here.')
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
 })
-
-
 
 gulp.task('watch', function () {
   watch('./app/index.html', function () {
